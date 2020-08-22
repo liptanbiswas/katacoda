25871e03485fc58a9dbea40e339e39064edff18c
Liptan Biswas <liptanbiswas@gmail.com>
2020-08-23 00:49:30 +0530

    Added verify command

---
+++ kube-test/step1-foreground.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+launch.sh
+cp /root/.kube/config /tmp/config && chmod 644 /tmp/config
--- kube-test/step1-verify.sh
+++ kube-test/step1-verify.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-
-cat <<EOF > check.sh
-#!/bin/bash
-set -o xtrace
-
-kubectl get ns | grep testns
-echo $?
-EOF
-
-chmod +x check.sh
-
-sh check.sh  2>&1 /tmp/out
+set -e
+KUBECONFIG=/root/.kube/config kubectl get ns | grep testns
+echo "done"
