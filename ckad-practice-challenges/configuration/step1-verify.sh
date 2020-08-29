191ee46053df1d0f3db76ad440461bc014522b16
Liptan Biswas <liptanbiswas@gmail.com>
2020-08-29 17:20:59 +0530

    Added fix for CKAD step 1

--- ckad-practice-challenges/configuration/step1-verify.sh
+++ ckad-practice-challenges/configuration/step1-verify.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-KUBECONFIG=/root/.kube/config kubectl get cm my-config -n database
+KUBECONFIG=/root/.kube/config kubectl get cm my-config -n datatab
 KUBECONFIG=/root/.kube/config kubectl get cm my-config -n datatab -o jsonpath='{.data.confa}' | grep -q -w exvalue
 echo "done"
