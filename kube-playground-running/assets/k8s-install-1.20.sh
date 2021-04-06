b846f6553a5cdc2dac6d09ed4efb4dd6503a866f
Liptan Biswas <liptanbiswas@gmail.com>
2021-04-06 21:14:22 +0530

    Added kubernetes apt repo

--- kube-playground-running/assets/k8s-install-1.20.sh
+++ kube-playground-running/assets/k8s-install-1.20.sh
@@ -4,6 +4,8 @@
 kubeadm config images pull --kubernetes-version 1.20.0 >>/tmp/master-upgrade.log 2>&1 &
 
 # Install master dependencies
+curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update -y >>/tmp/master-upgrade.log 2>&1
 apt-get install -y kubectl=1.20.0-00 kubeadm=1.20.0-00 kubelet=1.20.0-00 >>/tmp/master-upgrade.log 2>&1
 
